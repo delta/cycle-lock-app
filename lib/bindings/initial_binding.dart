1a0b1588b5f84ea67fa48d5e8862cd674311b868
muhesh <vmspride7@gmail.com>
2021-10-31 21:29:17 +0530

    fix:remove unused files

--- lib/bindings/initial_binding.dart
+++ lib/bindings/initial_binding.dart
@@ -1,11 +1,9 @@
 import 'package:cyclelock_app/api/api_manager.dart';
-import 'package:cyclelock_app/cache/cache_manager.dart';
 import 'package:get/instance_manager.dart';
 
 class InitialBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<ApiManager>(() => ApiManager());
-    Get.lazyPut<CacheManager>(() => CacheManager());
   }
 }
--- lib/cache/cache_manager.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get_storage/get_storage.dart';
-
-class CacheManager {
-  static final _cache = GetStorage('Cache');
-}
