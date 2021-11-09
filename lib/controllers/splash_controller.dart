332bafbfbe71700d8cb103c52ad170b900ff876c
Amarjit-Madhumalararungeethayan <m.a.amarjit@gmail.com>
2021-11-09 21:59:17 +0530

    Fix: Removed unused packages

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -1,8 +1,3 @@
-import 'dart:async';
-
-import 'package:cycle_lock/views/auth_page.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class SplashController extends GetxController {}
