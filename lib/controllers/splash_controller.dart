b9eb2475b3ea0a2920684b6b703422a71ad11884
Amarjit-Madhumalararungeethayan <m.a.amarjit@gmail.com>
2021-11-09 21:50:10 +0530

    Fix: Corrected Formatting

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -5,5 +5,4 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
-class SplashController extends GetxController {
-}
+class SplashController extends GetxController {}
