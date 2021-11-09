df31a910f044fa0677a70b011afdc594718db031
Subiksha Suresh <subikshasuresh@Subikshas-MacBook-Pro.local>
2021-11-09 23:08:14 +0530

    feat: implement login page

--- lib/views/auth_page.dart
+++ lib/views/auth_page.dart
@@ -1,12 +1,126 @@
 import 'package:cycle_lock/controllers/login_controller.dart';
+import 'package:cycle_lock/views/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 
 class AuthPage extends GetView<AuthController> {
   const AuthPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/login bg.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 40, top: 265),
+              child: const Text(
+                'Welcome User',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Moon',
+                    fontSize: 40),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.5,
+                    right: 35,
+                    left: 35),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade50,
+                        filled: true,
+                        hintText: 'Enter your e-mail',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 37,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey.shade50,
+                        filled: true,
+                        hintText: 'Enter your password',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontFamily: 'Moon',
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0x00000000),
+                          child: IconButton(
+                            color: Colors.white,
+                            onPressed: () => Get.to(const MainPage()),
+                            icon: const Icon(Icons.arrow_forward_ios_outlined),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Sign Up',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontFamily: 'Moon',
+                                color: Color(0xff4c505b),
+                                fontSize: 18),
+                          ),
+                          style: const ButtonStyle(),
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontFamily: 'Moon',
+                                fontSize: 18,
+                                color: Color(0xff4c505b),
+                              ),
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
