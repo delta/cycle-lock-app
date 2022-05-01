c0644c97f855fb2712ad240fa79fdb7f2b25e930
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-05-01 16:40:01 +0530

    basic functionality in SplashPage

--- lib/views/pages/splash_page.dart
+++ lib/views/pages/splash_page.dart
@@ -1,39 +1,65 @@
 import 'package:cycle_lock/views/themes/colors.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    checkAuthState();
+  }
+
+  Future<void> checkAuthState() async {
+    await Future.delayed(const Duration(seconds: 1));
+    print('Logged in');
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Stack(
           children: [
             Positioned(
-                top: 250,
-                bottom: 100,
-                left: 50,
-                right: 50,
-                child: Column(children: [
-                  Image.asset(
-                    'assets/images/lock.png',
-                  ),
+              top: 250,
+              bottom: 100,
+              left: 50,
+              right: 50,
+              child: Column(
+                children: [
+                  Image.asset('assets/images/lock.png'),
                   const SizedBox(height: 10),
-                  Text('Pad-Lock',
-                      style: TextStyle(fontSize: 35, shadows: <Shadow>[
+                  Text(
+                    'Pad-Lock',
+                    style: TextStyle(
+                      fontSize: 35,
+                      shadows: <Shadow>[
                         Shadow(
                           offset: const Offset(1.0, 1.0),
                           blurRadius: 1.0,
                           color: const AppColours().secondaryaccentcolor,
                         )
-                      ]))
-                ])),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
             const Positioned(
-                bottom: 50,
-                right: 50,
-                left: 50,
-                child: Center(
-                    child: Text('Made with 💚 by DeltaForce',
-                        style: TextStyle(fontSize: 18))))
+              bottom: 50,
+              right: 50,
+              left: 50,
+              child: Center(
+                child: Text(
+                  'Made with 💚 by DeltaForce',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            )
           ],
         ),
       );
