e4f1b569e46d9eeb2a45ae580a932e8b842f6996
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-05-01 18:59:17 +0530

    [fix]: set wantKeepAlive => false in ScanScreen to avoid MobileScanner bug

--- lib/views/pages/screens/scan_screen.dart
+++ lib/views/pages/screens/scan_screen.dart
@@ -1,57 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScanScreen extends StatelessWidget {
-  ScanScreen({Key? key}) : super(key: key);
+class ScanScreen extends StatefulWidget {
+  const ScanScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => false;
   final cameraController = MobileScannerController();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Mobile Scanner'),
-          actions: [
-            IconButton(
-              color: Colors.white,
-              icon: ValueListenableBuilder<TorchState>(
-                valueListenable: cameraController.torchState,
-                builder: (context, state, child) {
-                  switch (state) {
-                    case TorchState.off:
-                      return const Icon(Icons.flash_off, color: Colors.grey);
-                    case TorchState.on:
-                      return const Icon(Icons.flash_on, color: Colors.yellow);
-                  }
-                },
-              ),
-              iconSize: 32.0,
-              onPressed: cameraController.toggleTorch,
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobile Scanner'),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder<TorchState>(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
             ),
-            IconButton(
-              color: Colors.white,
-              icon: ValueListenableBuilder(
-                valueListenable: cameraController.cameraFacingState,
-                builder: (context, state, child) {
-                  switch (state as CameraFacing) {
-                    case CameraFacing.front:
-                      return const Icon(Icons.camera_front);
-                    case CameraFacing.back:
-                      return const Icon(Icons.camera_rear);
-                  }
-                },
-              ),
-              iconSize: 32.0,
-              onPressed: cameraController.switchCamera,
+            iconSize: 32.0,
+            onPressed: cameraController.toggleTorch,
+          ),
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear);
+                }
+              },
             ),
-          ],
-        ),
-        body: MobileScanner(
-          allowDuplicates: false,
-          controller: cameraController,
-          onDetect: (barcode, args) {
-            final code = barcode.rawValue;
-            debugPrint('Barcode found! $code');
-          },
-        ),
-      );
+            iconSize: 32.0,
+            onPressed: cameraController.switchCamera,
+          ),
+        ],
+      ),
+      body: MobileScanner(
+        allowDuplicates: false,
+        controller: cameraController,
+        onDetect: (barcode, args) {
+          final code = barcode.rawValue;
+          debugPrint('Barcode found! $code');
+        },
+      ),
+    );
+  }
 }
