9d1c33bf65fd7f8c8f2504c178c0c0f9828d0ec2
simmiggrwl <simmiggrwl@gmail.com>
2022-05-06 16:45:39 +0530

    profilePage

--- lib/views/pages/screens/profile_screen.dart
+++ lib/views/pages/screens/profile_screen.dart
@@ -1,9 +1,12 @@
+import 'package:cycle_lock/views/pages/screens/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('ProfilePage'));
+  Widget build(BuildContext context) => Container(
+        color: const Color.fromARGB(255, 243, 243, 243),
+        child: Center(child: profilePage()),
+      );
 }
