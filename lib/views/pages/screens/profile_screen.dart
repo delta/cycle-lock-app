80e4b5fa9e201e74b65e11ef58aec42a4caaefc4
simmiggrwl <simmiggrwl@gmail.com>
2022-05-06 23:37:35 +0530

    fix: add onClick callback

--- lib/views/pages/screens/profile_screen.dart
+++ lib/views/pages/screens/profile_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cycle_lock/views/pages/screens/profile_page.dart';
+import 'package:cycle_lock/views/pages/screens/profile_item.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -10,3 +10,72 @@ class ProfileScreen extends StatelessWidget {
         child: Center(child: profilePage()),
       );
 }
+
+Column profilePage() => Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 1, 47, 85), width: 3),
+                  ),
+                  child: const Center(
+                      child: Text(
+                    'JD',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 1, 47, 85),
+                        fontWeight: FontWeight.bold),
+                  ))),
+              const SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'John Doe',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 1, 47, 85),
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Text('106120119@nitt.edu',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.orange,
+                      ))
+                ],
+              )
+            ],
+          ),
+        ),
+        ProfileItem(
+            itemName: 'Edit Profile',
+            itemIcon: Icons.person,
+            itemColor: const Color.fromARGB(255, 1, 47, 85),
+            onClickAction: () => {}),
+        ProfileItem(
+            itemName: 'Settings',
+            itemIcon: Icons.settings,
+            itemColor: const Color.fromARGB(255, 1, 47, 85),
+            onClickAction: () => {}),
+        ProfileItem(
+            itemName: 'Feedback',
+            itemIcon: Icons.feedback,
+            itemColor: const Color.fromARGB(255, 1, 47, 85),
+            onClickAction: () => {}),
+        ProfileItem(
+            itemName: 'Logout',
+            itemIcon: Icons.logout,
+            itemColor: Colors.red,
+            onClickAction: () => {}),
+      ],
+    );
